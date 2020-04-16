82ab7c1c6878990da317f30371715c574bc3231b
William Mittard <billymittard@ymail.com>
2020-04-16 16:30:25 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,6 @@
 def my_hash
-  my_hash = Hash.new 
+  pets = Hash.new 
+  pets = { "cat" => "blue", "dog" => "max", "rabbit"
   
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
