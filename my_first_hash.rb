8b68cea451a18be1911cbbd4f52a2d204166d515
William Mittard <billymittard@ymail.com>
2020-04-16 16:37:27 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -16,10 +16,8 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  shipping_manifest = 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
-end
+  shipping_manifest = ["oil paintings"]
+  end
 
 def adding
   shipping_manifest = {
