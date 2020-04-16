70fc1f09ca52d7d45b985dbdf98e21e1a185a4bf
William Mittard <billymittard@ymail.com>
2020-04-16 16:43:37 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,7 +25,8 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  shipping_manifest = ["muskets" => 2,"gun powder" => 4]
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4
   
   shipping_manifest["muskets"]
   shipping_manifest["gun powder"]
